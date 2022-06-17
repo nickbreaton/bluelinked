f585ee7730cad51fe78dc66248d868cebf17e970
Nick Breaton <nick@breaton.com>
2022-06-16 23:36:29 -0400

    Add barebones example

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,5 +1,25 @@
 import type { Handle } from '@sveltejs/kit';
 
+import Bluelinky from 'bluelinky';
+import type { AmericanBlueLinkyConfig } from 'bluelinky/dist/controllers/american.controller';
+import type AmericanVehicle from 'bluelinky/dist/vehicles/american.vehicle';
+
 export const handle: Handle = async ({ event, resolve }) => {
+	const client = new Bluelinky<AmericanBlueLinkyConfig, 'US', AmericanVehicle>({
+		brand: 'hyundai',
+		region: 'US',
+		username: process.env.BLUELINK_USERNAME,
+		password: process.env.BLUELINK_PASSWORD,
+		pin: process.env.BLUELINK_PIN
+	});
+
+	await new Promise<void>((resolve, reject) => {
+		client.on('error', (error) => reject(error));
+		client.on('ready', () => resolve());
+	});
+
+	const [vehicle] = await client.getVehicles();
+	event.locals.vehicle = vehicle;
+
 	return await resolve(event);
 };
--- src/routes/status.ts
+++ src/routes/status.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const get: RequestHandler = () => {
+export const get: RequestHandler = async ({ locals }) => {
 	return {
-		body: 'OK'
+		body: JSON.stringify(await locals.vehicle.status({ parsed: false, refresh: false }), null, 2)
 	};
 };
